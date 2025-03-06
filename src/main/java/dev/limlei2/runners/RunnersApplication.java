1a41db450529ff957d20943a10f0526f07ce528f
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-06 18:07:33 -0500

    implemented postgres and spring data

--- src/main/java/dev/limlei2/runners/RunnersApplication.java
+++ src/main/java/dev/limlei2/runners/RunnersApplication.java
@@ -1,16 +1,9 @@
 package dev.limlei2.runners;
 
-import dev.limlei2.runners.run.Location;
-import dev.limlei2.runners.run.Run;
-import dev.limlei2.runners.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class RunnersApplication {
--- src/main/java/dev/limlei2/runners/run/Run.java
+++ src/main/java/dev/limlei2/runners/run/Run.java
@@ -2,17 +2,24 @@ package dev.limlei2.runners.run;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id,
-                  @NotEmpty
-                  String title,
-                  LocalDateTime startedOn,
-                  LocalDateTime completedOn,
-                  @Positive
-                  Integer miles,
-                  Location location) {
+public record Run(
+        @Id
+        Integer id,
+        @NotEmpty
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime completedOn,
+        @Positive
+        Integer miles,
+        Location location,
+        @Version
+        Integer version
+) {
     public Run{
         if(!completedOn.isAfter(startedOn)) {
             throw new IllegalArgumentException("Completed date must be after started date");
